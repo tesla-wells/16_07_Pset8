3fe192f6a6edda808d6d3e0cdb466f9ccba14a77
Tesla Wells <teslamwells@gmail.com>
2019-11-15 02:30:50 -0500

    Problem 1 complete

--- Problem1.m
+++ Problem1.m
@@ -1,6 +1,8 @@
+clear 
+close all
+
 k =1 ;
 m = 1;
-
 A = [ -2 1 0 0 0 0 0 0 0 ;
      1 -2 1 0 0 0 0 0 0 ;
      0 1 -2 1 0 0 0 0 0 ;
@@ -12,3 +14,12 @@ A = [ -2 1 0 0 0 0 0 0 0 ;
      0 0 0 0 0 0 0 1 -2 ];
  
  [vectors, eigenvalues] = eig(A);
+ 
+ 
+ for n = 1 : 4
+    figure(n)
+    hold on
+    i = 10-n;
+    title("plot of " + n + " mode")
+    scatter(linspace(1, 9, 9), vectors(:,i))
+ end
